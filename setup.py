fa56dbbc8b633322b07583f4e7c826c2ba46442e
Guillaume Viger <fenekku@users.noreply.github.com>
2025-03-25 16:05:37 -0400

    make invenio-sitemap a package useable by all

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 Northwestern University.
+#
+# invenio-sitemap is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Sitemap indices and sitemaps for InvenioRDM."""
+
+from setuptools import setup
+
+setup()

8cb2945a8b27980f1c8ce2c3929faeb933015730
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 11:01:34 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools

--- run-tests.sh
+++ run-tests.sh
@@ -40,7 +40,6 @@ if [[ ${keep_services} -eq 0 ]]; then
 	trap cleanup EXIT
 fi
 
-python -m check_manifest
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch2} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
 python -m pytest ${pytest_args[@]+"${pytest_args[@]}"}
 tests_exit_code=$?
